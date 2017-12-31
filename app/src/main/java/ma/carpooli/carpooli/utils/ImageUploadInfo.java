95011ecdd8be1d96415be99adb44c4c2a5904b1a
don-kakashi <idriss0said0alaoui@gmail.com>
2017-12-31 09:17:19 +0000

    images are stored to the firebase file storage

---
+++ app/src/main/java/ma/carpooli/carpooli/utils/ImageUploadInfo.java
@@ -0,0 +1,30 @@
+package ma.carpooli.carpooli.utils;
+
+/**
+ * Created by Cassandra on 12/31/2017.
+ */
+
+public class ImageUploadInfo {
+
+    public String imageName;
+
+    public String imageURL;
+
+    public ImageUploadInfo() {
+
+    }
+
+    public ImageUploadInfo(String name, String url) {
+
+        this.imageName = name;
+        this.imageURL= url;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+}
